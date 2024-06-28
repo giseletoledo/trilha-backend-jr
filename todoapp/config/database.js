ae126173f8f2c67dda444b2e6a314cc9332f1d8b
giseletoledo <gisesonia@gmail.com>
2024-06-28 14:41:09 -0300

    chore: configurando sqlite

---
+++ todoapp/config/database.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+});
+
+module.exports = sequelize;
